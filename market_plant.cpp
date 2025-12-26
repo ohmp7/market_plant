f2970db8a0f2571c75f5901de02a6cdf9a11c16b
Ohm Patel <ohmpatel@Ohms-MacBook-Pro.local>
2025-12-25 23:17:21 -0500

    server outline

--- market_plant.cpp
+++ market_plant.cpp
@@ -1,29 +1,46 @@
 #include <iostream>
 #include <cstdint>
+#include <unordered_map>
+#include <cstddef>
 
 using Price = std::int32_t;
 using Quantity = std::uint32_t;
+using InstrumentId = std::uint32_t;
+using Depth = std::size_t;
+using Levels = std::unordered_map<Price, Quantity>;
 
-enum class Side {
-    BUY,
-    SELL,
+enum class Side : std::uint8_t {
+    BID = 0,
+    ASK = 1,
 };
 
-struct Level {
-    Price level_price;
-    Quantity level_quantity;
+// Exchange -> Plant API
+struct Event {
+    std::uint64_t sequence_number;  // for UDP
+
+    InstrumentId instrument_id;
+    Side side;
+    Price price;  // if price dne, add a new level
+    Quantity quantity;  // if quantity == 0, erase level
+};
+
+// Plant -> Subscriber API (TBD)
+struct OrderBookDelta {
+
 };
 
 // use std::move later
 class OrderBook {
 public:
-    OrderBook(const std::vector<Level>& bids_, const std::vector<Level>& asks_, const std::string& ticker_)
-        : ticker(ticker_), bids(bids_), asks(asks_) {}
+    OrderBook(const Depth depth_,  const InstrumentId instrument_id_)
+        : instrument_id(instrument_id_), depth(depth_) {}
     
 private:
-    std::string ticker;
-    std::vector<Level> bids;
-    std::vector<Level> asks;
+    InstrumentId instrument_id;
+    Depth depth;
+
+    Levels bids;
+    Levels asks;
 };
 
 int main() {
