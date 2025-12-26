925aea922b0cafa043c6a2e47882b4e4698af291
Ohm Patel <ohmpatel@Ohms-MBP.fios-router.home>
2025-12-26 14:30:11 -0500

    conversions

--- moldudp64_client.cpp
+++ moldudp64_client.cpp
@@ -1,3 +1,4 @@
+#include <arpa/inet.h>ß
 #include <cstdint>
 #include <exception>
 #include <iostream>
@@ -11,9 +12,9 @@ public:
         : message_("Packet Truncated Error: received " + std::to_string(received) +
                    " bytes, but was expecting >= " + std::to_string(expected)) {}
     
-    const char* what() const noexcept override {
+    virtual const char* what() const noexcept override {
         return message_.c_str();
-    }       
+    }
 private:
     std::string message_;
 };
@@ -23,20 +24,76 @@ struct SequenceGap {
     std::uint64_t request_until_sequence_num = 0;
 };
 
+// convert to template later -> read_big_endian
+static std::uint64_t read_u64_big_endian(const std::uint8_t* buf,  Bytes offset) {
+    std::uint64_t converted = 0;
+
+    for (size_t i = 0; i < 8; ++i) {
+        // shift left 8 bits
+        converted = converted << 8;
+
+        // get next byte from buf
+        std::uint8_t next_byte = buf[offset + i];
+
+        // OR operation with lower 8 bits
+        converted = converted | static_cast<std::uint64_t>(next_byte);
+    }
+
+    return converted;
+}
+
+static std::uint16_t read_u16_big_endian(const std::uint8_t* buf,  Bytes offset) {
+    std::uint64_t converted = 0;
+
+    for (size_t i = 0; i < 2; ++i) {
+        // shift left 8 bits
+        converted = converted << 8;
+
+        // get next byte from buf
+        std::uint8_t next_byte = buf[offset + i];
+
+        // OR operation with lower 8 bits
+        converted = converted | static_cast<std::uint64_t>(next_byte);
+    }
+
+    return converted;
+}
+
 class MoldUDP64 {
 public:
     MoldUDP64(std::uint64_t request_sequence_num_)
-        : expected_request_sequence_num(request_sequence_num_) {}
+        : expected_sequence_num(request_sequence_num_) {}
 
-    void handle_packet() {}
-    void end_session() {}
+    void handle_packet(const std::uint8_t* buf, Bytes len) {
+        // MoldUDP64 header truncate check
+        if (len < HEADER_LENGTH) throw PacketTruncatedError(len, HEADER_LENGTH);
+
+        Bytes curr_offset;
+
+        // get first 10 bytes for session (endian conversion is not needed)
+
+        // get the next 8 bytes for sequence number and convert to endian 
+
+        // get the next 2 bytes for message count (should always be 1 for now)
+
+        // current_sequence_number = sequence_number + message_count
+
+        // check 
+        // if current_sequence_number >
+    }
+
+    void end_session() {
+        gap.active = false;
+        gap.request_until_sequence_num = 0;
+    }
 
 private:
     static constexpr Bytes SESSION_LENGTH = 10;
+    static constexpr Bytes HEADER_LENGTH = 20;
     static constexpr std::uint16_t TIMEOUT = 1000;  // ms
+    static constexpr std::uint16_t END_SESSION = 0xFFFF;
 
     char session[SESSION_LENGTH]{};
-    std::uint64_t expected_request_sequence_num;
+    std::uint64_t expected_sequence_num;
     SequenceGap gap;
-
 };
