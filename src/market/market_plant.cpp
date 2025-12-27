f32fb29e31ffb7821266ce277e8059f1cb3099c8
Ohm Patel <ohmpatel@Ohms-MBP.fios-router.home>
2025-12-27 18:34:13 -0500

    define exchange payload

--- src/market/market_plant.cpp
+++ src/market/market_plant.cpp
@@ -14,9 +14,20 @@ enum class Side : std::uint8_t {
     ASK = 1,
 };
 
-// Exchange -> Plant API
+/* 
+
+Exchange -> Plant API Payload (Network-Byte-Order / Big Endian)
+
+offset 20 - 21:     msg_len         (2 bytes, u16)        ex. 'msg_len' = 18 (bytes)
+offset 22 - 25:     instrument_id   (4 bytes, u32)        ex. 'AAPL' = 1
+offset 26:          side            (1 byte, u8)          ex. 'BID' = 0, 'ASK' = 1
+offset 27:          event           (1 byte, u8)          ex. 'ADD' = 0, 'REDUCE' = 1
+offset 28 - 31:     price           (4 bytes, u32)        ex. 'price' = 32 (USD)
+offset 32 - 39:     exchange_ts     (8 bytes, u64)        ex. 1234567891234567890 (ns)
+
+*/
+
 struct Event {
-    std::uint64_t sequence_number;  // for UDP
     InstrumentId instrument_id;
     Side side;
     Price price;  // if price dne, add a new level
