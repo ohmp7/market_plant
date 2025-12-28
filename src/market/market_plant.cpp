0a942e2375463dfc373d0c9a539c32cd3830fb8b
Ohm Patel <ohmpatel@Ohms-MacBook-Pro.local>
2025-12-28 12:17:24 -0500

    fix

--- src/market/market_plant.cpp
+++ src/market/market_plant.cpp
@@ -1,9 +1,15 @@
 #include "market_plant.h"
 
+#include <iostream>
+#include <thread>
+
+
 OrderBook::OrderBook(const Depth depth_,  const InstrumentId instrument_id_)
     : instrument_id(instrument_id_), depth(depth_) {}
 
 
 int main(int argc, char* argv[]) {
+
+    std::cout << "hello word!" << "\n";
     return 0;
 }
